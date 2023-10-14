18601cfe3b28b18a3b255f08c571acec673e8004
Ertekin <ozclkertekin@gmail.com>
2023-10-14 13:39:21 +0300

    pagination

---
+++ src/components/getSearchParamFromURL.js
@@ -0,0 +1,10 @@
+export default function getSearchParamFromURL(url, param) {
+  const include = url.includes(param);
+
+  if (!include) return null;
+
+  const params = url.split(/([&,?,=])/);
+  const index = params.indexOf(param);
+  const value = params[index + 2];
+  return value;
+}
--- src/screens/ProductScreen/ProductScreen.js
+++ src/screens/ProductScreen/ProductScreen.js
@@ -1,15 +1,16 @@
-import {View, Text, StyleSheet, FlatList, ActivityIndicator} from 'react-native';
+import {View, Text, StyleSheet, FlatList, ActivityIndicator, Button} from 'react-native';
 import React, {useEffect, useState} from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import CustomTouchableOpacity from '../../components/touchableOpacity';
 import axios from 'axios';
 import navStrings from '../../constants/navStrings';
 import literals from '../../constants/literals';
+import getSearchParamFromURL from '../../components/getSearchParamFromURL';
 
-const InnerComponent = ({item}) => {
+const InnerComponent = ({item, textStyle, viewStyle}) => {
   return (
-    <View style={styles.txtBoxStyle}>
-      <Text style={styles.titleStyle}>{item.name.toUpperCase()}</Text>
+    <View style={[styles.txtBoxStyle, viewStyle]}>
+      <Text style={[styles.titleStyle, textStyle]}>{item.toUpperCase()}</Text>
     </View>
   );
 };
@@ -26,17 +27,29 @@ const pokeNavFunc = (nav, pokeData) => {
 
 const ProductScreen = ({navigation}) => {
   const url = literals.POKE_LIST_URL;
+  const [currentUrl, setCurrentUrl] = useState(literals.POKE_LIST_URL);
   const [poke, setPoke] = useState([]);
   const [loading, setLoading] = useState(true);
   const [hasError, setErrorFlag] = useState(false);
+  const [nextPokeUrl, setNextPokeUrl] = useState();
+  const [prevPokeUrl, setPrevPokeUrl] = useState();
+  const [offset, setOffSet] = useState();
 
   async function getPokeAxios(controller) {
     axios
-      .get(url, {signal: controller.signal})
+      .get(currentUrl, {signal: controller.signal})
       .then(response => {
         console.log('\n');
         console.log('fetching poke data...');
         setPoke(response.data.results);
+        setNextPokeUrl(response.data.next);
+        setPrevPokeUrl(response.data.previous);
+
+        console.log(response.data.next);
+
+        const params = getSearchParamFromURL(response.data.next, 'offset');
+        console.log('params: ', params);
+        setOffSet(params);
       })
       .catch(err => {
         if (controller.abort) {
@@ -53,6 +66,16 @@ const ProductScreen = ({navigation}) => {
       });
   }
 
+  function goToNext() {
+    setCurrentUrl(nextPokeUrl);
+  }
+  function goToPrev() {
+    setCurrentUrl(prevPokeUrl);
+  }
+  function goToTop() {
+    setCurrentUrl(url);
+  }
+
   useEffect(() => {
     const controller = new AbortController();
     getPokeAxios(controller);
@@ -61,27 +84,61 @@ const ProductScreen = ({navigation}) => {
       console.log('fetching pokemon detail ended!');
       controller.abort;
     };
-  }, [url, hasError]);
+  }, [hasError, currentUrl]);
 
   return (
     <SafeAreaView style={styles.container}>
       {loading ? (
         <ActivityIndicator size="large" />
       ) : (
-        <FlatList
-          data={poke}
-          renderItem={({item}) => (
-            <CustomTouchableOpacity
-              activeOpacity={0.8}
-              onPress={() => {
-                pokeNavFunc(navigation, item);
-              }}
-              styles={styles.buttonStyle}>
-              <InnerComponent item={item} />
-            </CustomTouchableOpacity>
-          )}
-          keyExtractor={item => item.url}
-        />
+        <View style={styles.container}>
+          <FlatList
+            data={poke}
+            renderItem={({item}) => (
+              <CustomTouchableOpacity
+                activeOpacity={0.8}
+                onPress={() => {
+                  pokeNavFunc(navigation, item);
+                }}
+                styles={styles.buttonStyle}>
+                <InnerComponent item={item.name} />
+              </CustomTouchableOpacity>
+            )}
+            keyExtractor={item => item.url}
+          />
+          <View style={styles.paginationButtonContainer}>
+            {prevPokeUrl && (
+              <CustomTouchableOpacity
+                activeOpacity={0.8}
+                onPress={() => {
+                  goToPrev();
+                }}
+                buttonStyle={styles.buttonStyle}>
+                <InnerComponent item={'< Prev'} textStyle={{textAlign: 'center'}} />
+              </CustomTouchableOpacity>
+            )}
+            {offset > 100 && (
+              <CustomTouchableOpacity
+                activeOpacity={0.8}
+                onPress={() => {
+                  goToTop();
+                }}
+                buttonStyle={styles.buttonStyle}>
+                <InnerComponent item={'Reset!'} textStyle={{textAlign: 'center'}} />
+              </CustomTouchableOpacity>
+            )}
+            {nextPokeUrl && (
+              <CustomTouchableOpacity
+                activeOpacity={0.8}
+                onPress={() => {
+                  goToNext();
+                }}
+                buttonStyle={styles.buttonStyle}>
+                <InnerComponent item={'Next >'} textStyle={{textAlign: 'center'}} />
+              </CustomTouchableOpacity>
+            )}
+          </View>
+        </View>
       )}
     </SafeAreaView>
   );
@@ -90,6 +147,17 @@ const ProductScreen = ({navigation}) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+
+  paginationButtonContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-evenly',
+    height: '7%',
+  },
+  buttons: {
+    width: '12%',
+    height: '12%',
   },
 
   listContainer: {
@@ -115,11 +183,24 @@ const styles = StyleSheet.create({
   },
 
   buttonStyle: {
-    padding: 40,
-    borderRadius: 4,
-    borderWidth: 1,
-    borderColor: 'green',
+    padding: 2.5,
+    margin: 3.5,
+    borderRadius: 0,
+    // height:'100%',
+    // borderWidth: 0.5,
+    // borderColor: 'green',
   },
 });
 
 export default ProductScreen;
+
+/*   const getSearchParamFromURL = (url, param) => {
+    const include = url.includes(param);
+
+    if (!include) return null;
+
+    const params = url.split(/([&,?,=])/);
+    const index = params.indexOf(param);
+    const value = params[index + 2];
+    return value;
+  }; */
